32b7c41bd684b063f89bd74a9690fa4086b223ad
alexpko <alexpko94@gmail.com>
2021-09-29 10:49:36 +0300

    added webView for books

--- app/src/main/java/com/example/new_yourk_times_api/ui/books/BooksDetailFragment.kt
+++ app/src/main/java/com/example/new_yourk_times_api/ui/books/BooksDetailFragment.kt
@@ -1,8 +1,50 @@
 package com.example.new_yourk_times_api.ui.books
 
+import android.os.Bundle
+import android.view.*
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.new_yourk_times_api.databinding.FragmentBooksDetailBinding
 
 
 class BooksDetailFragment : Fragment() {
+    private lateinit var binding: FragmentBooksDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val args = BooksDetailFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "NumCorrect: ${args.booksUrl}", Toast.LENGTH_LONG).show()
+
+        binding = FragmentBooksDetailBinding.inflate(inflater)
+        setUpWebView(args.booksUrl)
+        return binding.root
+    }
+
+    private fun setUpWebView (url: String) {
+        val wbWebview : WebView = binding.wbWebView
+        wbWebview.loadUrl(url)
+
+        val webSetting = wbWebview.settings
+        webSetting.javaScriptEnabled = true
+
+        wbWebview.webViewClient = WebViewClient()
+
+        wbWebview.canGoBack()
+        wbWebview.setOnKeyListener(View.OnKeyListener{ v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.action == MotionEvent.ACTION_UP
+                && wbWebview.canGoBack()){
+                wbWebview.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
+    }
 
 }
--- app/src/main/java/com/example/new_yourk_times_api/ui/books/BooksListFragment.kt
+++ app/src/main/java/com/example/new_yourk_times_api/ui/books/BooksListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.new_yourk_times_api.data.network.books.BooksApiService
 import com.example.new_yourk_times_api.data.network.books.NyTimesApiBooks
@@ -70,13 +71,10 @@ class BooksListFragment : Fragment(), BooksView {
         }
     }
     private fun onBooksClick(books: Books) {
-        Snackbar.make(binding.root, "CliCKED", Snackbar.LENGTH_SHORT).show()
-        /*val newsUrl = books.urlAmazon
         view?.findNavController()
             ?.navigate(
-                NewsListFragmentDirections
-                .actionNewsListFragmentToNewsDetailFragment(newsUrl))*/
+                BooksListFragmentDirections
+                .actionBooksListFragmentToBooksDetailFragment(books.urlAmazon))
     }
-
 
 }
